1167c90f539c5f28f1b80ca8aac3f6d321dfb3dd
bujing88 <jungbk03@kumoh.ac.kr>
2022-09-23 09:24:51 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ def print_hello():
     print("Hello World!!")
 
 def print_KIT():
-    print("KIT")
+    print("Kumoh National Institute of Technology")
 
 if __name__ == '__main__':
     print_hello()
